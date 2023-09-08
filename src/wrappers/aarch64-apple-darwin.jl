dcfc60c2347415cee95456d9aed71e08346ef2d1
jlbuild <juliabuildbot@gmail.com>
2023-09-08 18:54:29 +0000

    Tar_jll build 1.35.0+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -2,10 +2,11 @@
 export tar
 
 using Attr_jll
+using Libiconv_jll
 JLLWrappers.@generate_wrapper_header("Tar")
 JLLWrappers.@declare_executable_product(tar)
 function __init__()
-    JLLWrappers.@generate_init_header(Attr_jll)
+    JLLWrappers.@generate_init_header(Attr_jll, Libiconv_jll)
     JLLWrappers.@init_executable_product(
         tar,
         "bin/tar",
